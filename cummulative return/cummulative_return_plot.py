2070fa12dfa937b16446b8c1e7457b43d2406197
Harishangaran Krishnamoorty <54322105+Harishangaran@users.noreply.github.com>
2020-03-28 19:22:53 +0000

    Update cummulative_return_plot.py

--- cummulative return/cummulative_return_plot.py
+++ cummulative return/cummulative_return_plot.py
@@ -8,6 +8,10 @@ The following script plots the cummulative return of stocks.
 
 Call the class and input your list of stocks and period to plot
 the cummulative graph.
+    eg: cummulativeReturn(['AAPL','GOOG','FB','AMZN'],'252d').plotGraph(size=(16,6))
+
+Change the size of the graph by changing the size attribute.
+By default it is (16,6)
 
 Visualise how much $1 invested over a period would have returned.
 
@@ -52,10 +56,11 @@ class cummulativeReturn:
         self.listToPlot = pd.concat(listOfCummReturns,axis=1)
         self.listToPlot.columns = self.stocklist
         
-    def plotGraph(self):
+    def plotGraph(self,size=(16,6)): #must give a default value for args
+        self.size = size
         plt.style.use('ggplot')
-        self.listToPlot.plot(figsize=(16,6))
+        self.listToPlot.plot(figsize=(self.size))
         plt.title('How much $1 invested in these stocks returns you over a period of {}'.format(self.period))
         plt.ylabel('($)')
         
-cummulativeReturn(['AAPL','GOOG','FB','AMZN'],'252d')
+cummulativeReturn(['AAPL','GOOG','FB','AMZN'],'252d').plotGraph((16,6))
